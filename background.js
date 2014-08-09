fe584bc4c557021580c0490e8ad98f27e0dcfa5d
Douglas Bentley <bentles@localhost.localdomain>
2014-08-09 09:58:15 +0200

    Shift+Enter to playlist search
    Also killed the silly icon and replaced with the word "Playlist". Looks fine on chrome in linux.

--- background.js
+++ background.js
@@ -1,9 +1,24 @@
 $(document).ready(function(){
-    var $button = $('<button class="yt-uix-button yt-uix-button-size-default yt-uix-button-default search-btn-component" type="button"><span class="yt-uix-button-content">Search</span><span class="yt-uix-button-content">Search</span></button>');
+    var $button = $('<button class="yt-uix-button yt-uix-button-size-default yt-uix-button-default search-btn-component" type="button">Playlist</button>');
     $button.insertBefore($("#search-btn"));
+
+    
+    $("#masthead-search-term").keyup(function(event){
+	if (event.shiftKey && event.which === 13)
+	{
+	    plSearch();
+	    return false;
+	}
+    });    
+
     $button.click(function(){
+	plSearch();
+    });
+
+    function plSearch()
+    {
 	var search = encodeURIComponent($("#masthead-search-term").val());
 	search = "results?search_query="+ search +"&filters=playlist";
 	window.location.replace(search);
-    });
+    }
 });
