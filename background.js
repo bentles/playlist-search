fdb2bc90444314bd1aaaa1c033a0533244f389fc
bentles <dee.bent.lee@gmail.com>
2014-08-10 16:49:36 +0200

    accounted for empt search

--- background.js
+++ background.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
     var $button = $('<button class="yt-uix-button yt-uix-button-size-default yt-uix-button-default search-btn-component" type="button">Playlist</button>');
     $button.insertBefore($("#search-btn"));
-
     
     $("#masthead-search-term").keyup(function(event){
 	if (event.shiftKey && event.which === 13)
@@ -17,8 +16,11 @@ $(document).ready(function(){
 
     function plSearch()
     {
-	var search = encodeURIComponent($("#masthead-search-term").val());
-	search = "results?search_query="+ search +"&filters=playlist";
-	window.location.replace(search);
+	var search = $.trim(encodeURIComponent($("#masthead-search-term").val()));
+	if (search != "")
+	{
+	    search = "results?search_query="+ search +"&filters=playlist";
+	    window.location.replace(search);
+	}
     }
 });
