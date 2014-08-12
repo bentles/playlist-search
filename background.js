749c1bb87b0a72e4c286a88f55a694391895bb20
Douglas Bentley <bentles@localhost.localdomain>
2014-08-12 09:46:19 +0200

    manifest changed. not sure about what happened to background.js

--- background.js
+++ background.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
     var $button = $('<button class="yt-uix-button yt-uix-button-size-default yt-uix-button-default search-btn-component" type="button">Playlist</button>');
     $button.insertBefore($("#search-btn"));
-
     
     $("#masthead-search-term").keyup(function(event){
 	if (event.shiftKey && event.which === 13)
